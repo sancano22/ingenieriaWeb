9bb24975246b2a07935dee6afe460cd5bcd6e74c
Cliqui-robot <roboticaeducativapucv@gmail.com>
2023-03-21 19:26:48 -0300

    practica de clase de introducción etiquetas html5 + css + js

---
+++ front-end/practica (HTML+CSS+JS)/js/inicio.js
@@ -0,0 +1,17 @@
+//funcion de prueba
+function Imprimir(){
+   console.log("Hola Chiquill@s");
+   
+   var nombre="Hola";
+
+   var nombre=1;
+
+   var nombre='C';
+
+   console.log(nombre);
+
+   var salida=document.getElementById("titulo");
+
+   salida.innerHTML="Pueden irse";
+   
+}
