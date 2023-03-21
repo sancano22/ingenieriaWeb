d221f77259235ed09b34e07ef460116c9d6d9bc0
Cliqui-robot <roboticaeducativapucv@gmail.com>
2023-03-21 19:30:52 -0300

    laboratorio 1

---
+++ Laboratorios/laboratorio1/js/inicio.js
@@ -0,0 +1,11 @@
+function Imprimir(){
+    console.log("Hola, bienvenidos");
+    /*var hola="asasa";
+    var hola=1;
+    console.log(hola);*/
+    var titulo=document.getElementById("titulo");
+    console.log(titulo);
+    titulo.innerHTML="Hola, bienvenidos";
+
+
+}
